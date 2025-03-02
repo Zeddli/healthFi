b6e9ac61cc43faf174b3fda348a7e960c996c786
Zhijian <ruj8320@gmail.com>
2025-03-02 11:44:25 +0800

    configure network details

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
@@ -7,12 +8,12 @@ module.exports = {
     vanguard: {
       url: "https://rpc-vanguard.vanarchain.com",
       chainId: 78600,
-      accounts: [ "dca09f11788e7954a8982f421ec3617f7fc9901a51af2dc6cc612a90c15c1b6f" ]
+      accounts: [ process.env.PRIVATE_KEY ]
     },
     mainnet: {
       url: "https://rpc.vanarchain.com",
       chainId: 2040,
-      accounts: [ "dca09f11788e7954a8982f421ec3617f7fc9901a51af2dc6cc612a90c15c1b6f" ]
+      accounts: [ process.env.PRIVATE_KEY ]
     }
   }
 };
