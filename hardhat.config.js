ac343e458d48d8caca94de1e62dce52b17c9e2b3
Zhijian <ruj8320@gmail.com>
2025-03-02 22:11:58 +0800

    frontend setup

--- hardhat.config.js
+++ hardhat.config.js
@@ -15,5 +15,12 @@ module.exports = {
       chainId: 2040,
       accounts: [ process.env.PRIVATE_KEY ]
     }
-  }
+  },
+  etherscan: {
+    // Use your Vanar Chain API key if provided by the explorer
+    apiKey: {
+      vanguard: "", //process.env.VANAR_API_KEY ||
+      mainnet: "", //process.env.VANAR_API_KEY ||
+    },
+  },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,17 +1,20 @@
+const { ethers } = require("hardhat");
+
 async function main() {
-    const ConsentManager = await ethers.getContractFactory("ConsentManager");
-    console.log("Deploying ConsentManager...");
-  
-    const consentManager = await ConsentManager.deploy();
-    // No need to call deployed() if deploy() is already awaited.
-    
-    console.log("ConsentManager deployed to:", consentManager.address);
-  }
-  
-  main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-      console.error(error);
-      process.exit(1);
-    });
-  
+  const ConsentManager = await ethers.getContractFactory("ConsentManager");
+  console.log("Deploying ConsentManager...");
+  const consentManager = await ConsentManager.deploy();
+
+  // Wait for the deployment transaction to be mined
+  await consentManager.deployTransaction.wait();
+
+  console.log("ConsentManager deployed to:", consentManager.target);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
